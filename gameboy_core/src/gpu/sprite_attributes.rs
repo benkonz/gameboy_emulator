9f27c432ba557959f461c474e9b2c97ec7090120
konz <benjamin.konz@impact.com>
2019-11-24 15:49:34 -0500

    fix bug in sprite colors

--- gameboy_core/src/gpu/sprite_attributes.rs
+++ gameboy_core/src/gpu/sprite_attributes.rs
@@ -1,5 +1,8 @@
 bitflags! {
     pub struct SpriteAttributes : u8 {
+        const PALETTE_NUMBER_BIT_0  = 0b0000_0001;
+        const PALETTE_NUMBER_BIT_1  = 0b0000_0010;
+        const PALETTE_NUMBER_BIT_2  = 0b0000_0100;
         const VRAM_BANK             = 0b0000_1000;
         const PALETTE               = 0b0001_0000;
         const X_FLIP                = 0b0010_0000;

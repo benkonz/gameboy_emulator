5dd977a424846043fcb8fedf01dd9736506754de
konz <benjamin.konz@impact.com>
2019-11-25 08:45:00 -0500

    fixed blaarg tests

--- gameboy_core/src/cpu/registers/flag.rs
+++ gameboy_core/src/cpu/registers/flag.rs
@@ -5,9 +5,5 @@ bitflags! {
         const NEGATIVE      = 0b0100_0000;
         const HALF_CARRY    = 0b0010_0000;
         const FULL_CARRY    = 0b0001_0000;
-        const UNUSED_4      = 0b0001_0000;
-        const UNUSED_3      = 0b0000_0100;
-        const UNUSED_2      = 0b0000_0010;
-        const UNUSED_1      = 0b0000_0001;
     }
 }
